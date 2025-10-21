8cc7b33f330c40c4416df79b81dac1f6c01c20f0
Storm13Cloud <69747902+Storm13Cloud@users.noreply.github.com>
2025-10-21 14:52:56 -0400

    Add files via upload

---
+++ KeyPins.h
@@ -0,0 +1,19 @@
+#ifndef PIN_DEFINITIONS_H
+#define PIN_DEFINITIONS_H
+
+#include <Arduino.h>
+
+const uint8_t KEY1 = 0;
+const uint8_t KEY2 = 1;
+const uint8_t KEY3 = 2;
+const uint8_t KEY4 = 3;
+const uint8_t KEY5 = 4;
+const uint8_t KEY6 = 5;
+const uint8_t KEY7 = 8;
+const uint8_t KEY8 = 9;
+const uint8_t KEY9 = 10;
+const uint8_t KEY10 = 11;
+const uint8_t KEY11 = 12;
+const uint8_t KEY12 = 13;
+
+#endif // PIN_DEFINITIONS_H
