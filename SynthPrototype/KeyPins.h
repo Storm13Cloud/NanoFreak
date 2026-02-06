8e911f6c76bc8dfc2761b33db0c157ba1a48b2ab
Storm13Cloud <69747902+Storm13Cloud@users.noreply.github.com>
2026-02-06 18:42:17 -0500

    Add files via upload

---
+++ SynthPrototype/KeyPins.h
@@ -0,0 +1,23 @@
+#ifndef PIN_DEFINITIONS_H
+#define PIN_DEFINITIONS_H
+
+#include <Arduino.h>
+
+const uint8_t KEY1 = 8;
+const uint8_t KEY2 = 9;
+const uint8_t KEY3 = 10;
+const uint8_t KEY4 = 11;
+const uint8_t KEY5 = 12;
+const uint8_t KEY6 = 13;
+const uint8_t KEY7 = 0;
+const uint8_t KEY8 = 1;
+const uint8_t KEY9 = 2;
+const uint8_t KEY10 = 3;
+const uint8_t KEY11 = 4;
+const uint8_t KEY12 = 5;
+const uint8_t LPLED = 15;
+const uint8_t BPLED = 6;
+const uint8_t HPLED = 7;
+const uint8_t FILTERTYPE = 14;
+
+#endif // PIN_DEFINITIONS_H
